0fd40674bceb83576c8adb98c871a3ae05bbc31f
Hunriken <hunriken@gmail.com>
2026-01-19 02:45:09 +0800

    启用dark

--- blog.config.js
+++ blog.config.js
@@ -12,7 +12,7 @@ const BLOG = {
 
   PSEUDO_STATIC: process.env.NEXT_PUBLIC_PSEUDO_STATIC || false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
   NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 60, // 更新缓存间隔 单位(秒)；即每个页面有60秒的纯静态期、此期间无论多少次访问都不会抓取notion数据；调大该值有助于节省Vercel资源、同时提升访问速率，但也会使文章更新有延迟。
-  APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
+  APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'dark', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || false, // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
   BACKGROUND_LIGHT: process.env.NEXT_PUBLIC_BACKGROUND_LIGHT || '#0e1116', // 日间模式背景色（深色背景）
   BACKGROUND_DARK: process.env.NEXT_PUBLIC_BACKGROUND_DARK || '#0a0a0a', // 深色模式背景色
