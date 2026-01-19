ecef0e3d6595a2ae2c6bbfc21bfdd53af5bce61d
Hunriken <hunriken@gmail.com>
2026-01-19 18:01:06 +0800

    开屏文字，打字速度。标题修改notion标题，欢迎语查看notion config

--- themes/matery_custom/components/Hero.js
+++ themes/matery_custom/components/Hero.js
@@ -26,9 +26,9 @@ const Hero = props => {
           changeType(
             new window.Typed('#typed', {
               strings: GREETING_WORDS,
-              typeSpeed: 200,
-              backSpeed: 100,
-              backDelay: 400,
+              typeSpeed: 60,
+              backSpeed: 30,
+              backDelay: 600,
               showCursor: true,
               smartBackspace: true
             })
@@ -57,11 +57,11 @@ const Hero = props => {
       className=' w-full h-screen relative bg-black'>
       <div className='text-white absolute flex flex-col h-full items-center justify-center w-full '>
         {/* 站点标题 */}
-        <div className='text-4xl md:text-5xl z-20 shadow-text'>
+        <div className='text-6xl md:text-8xl z-20 '>
           {siteInfo?.title || siteConfig('TITLE')}
         </div>
         {/* 站点欢迎语 */}
-        <div className='mt-2 h-12 items-center text-center z-20 shadow-text text-white text-lg'>
+        <div className='mt-4 h-16 items-center text-center z-20 text-white text-lg'>
           <span id='typed' />
         </div>
         {/* 滚动按钮 */}
